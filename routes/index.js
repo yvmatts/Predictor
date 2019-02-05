297a5117adc08aeb61a916a7e0f6e30866e8249b
Yashvardhan Mathur <work.yvm@gmail.com>
2019-02-05 11:33:52 +0530

    Test Commit

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var data = {name:"Yash", age:"21"};
+
+
   res.render('index', { title:[
               {name:"Yash"},
               {name:"Ajay"}
