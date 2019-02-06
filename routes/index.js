c86c9e7e30d76954c6d49a0a7ba95844ee50f10e
Yashvardhan Mathur <work.yvm@gmail.com>
2019-02-06 19:37:33 +0530

    File Upload Created.

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '/home/yvmatts/Documents/Development/Predictor');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now());
+  }
+});
+
+var upload = multer({ storage: storage });
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  var data = {name:"Yash", age:"21"};
-
-
-  res.render('index', { title:[
-              {name:"Yash"},
-              {name:"Ajay"}
-            ]
-      });
+router.get('/home', function(req, res, next) {
+//  console.log(req.file);
+  res.render('index');
 });
+
+router.post('/home',upload.single('img'), function(req, res, next) {
+  console.log(req.file.mimetype);
+//  res.send("File Uploaded");
+res.send(req.file.mimetype);
+});
+
+
 
 module.exports = router;
